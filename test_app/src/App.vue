5744b33e99bce8bc0d9b13b953a9fef2aed3e9a1
ZachyDev <mosetizachary001@gmail.com>
2020-10-08 18:16:23 +0300

    first example of the composition api

---
+++ test_app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <TestVue3 msg="Welcome to Your Vue.js App"/>
+    <Test2 />
+  </div>
+  
+</template>
+
+<script>
+import TestVue3 from './components/TestVue3.vue'
+import Test2 from '@/components/Test2.vue'
+export default {
+  name: 'App',
+  components: {
+    TestVue3,
+    Test2
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ test_app/src/components/Test2.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h2>{{ capacity }}</h2>
+      <button @click="increaseCapacity()">Increase Capacity</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+    setup() {
+        const capacity = ref(0);
+        function increaseCapacity() {
+            capacity.value++;
+        }
+        return { capacity, increaseCapacity };
+    }
+}
+</script>
+
+<style>
+
+</style>
