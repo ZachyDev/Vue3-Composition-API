8ccff0aa08be9a1df958f07796f8a64776005b86
ZachyDev <mosetizachary001@gmail.com>
2020-10-08 18:28:20 +0300

    fetched data from a third party api

--- test_app/src/components/Test2.vue
+++ test_app/src/components/Test2.vue
@@ -1,19 +1,31 @@
 <template>
-  <div>
-      <h2>{{ capacity }}</h2>
-      <button @click="increaseCapacity()">Increase Capacity</button>
+  <div> 
+      <h2> Geeky Sesion by {{ name }}.</h2>
+      <button @click="increaseCapacity()">Increase Capacity</button> <span>{{ capacity }}</span>
+      <button @click="getData()">Fetch Data</button>
+      <h2>{{companies}}</h2>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
+import axios from 'axios';
 export default {
     setup() {
         const capacity = ref(0);
+        const companies = ref('hello');
+        const name = ref('ZachyDev');
+        // const companies = ref('https://my-json-server.typicode.com/ZachyDev/ZachyAPI/companies');
         function increaseCapacity() {
             capacity.value++;
         }
-        return { capacity, increaseCapacity };
+        function getData() {
+            axios.get('https://my-json-server.typicode.com/ZachyDev/ZachyAPI/companies')
+                .then(res => {
+                    companies.value = res.data;
+                })
+        } 
+        return { name, capacity, increaseCapacity,getData, companies };
     }
 }
 </script>
